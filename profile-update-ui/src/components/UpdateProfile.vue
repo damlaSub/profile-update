ad63f04b05f304058b5007144035daf4b6123aca
DamlaSub <mme.dalloway@gmail.com>
2023-04-23 22:56:37 +0200

    add event handlers + solve maxValue problem

--- profile-update-ui/src/components/UpdateProfile.vue
+++ profile-update-ui/src/components/UpdateProfile.vue
@@ -1,21 +1,41 @@
 <script>
 import { useVuelidate } from '@vuelidate/core'
-//keep going with https://vuelidate-next.netlify.app/#alternative-syntax-composition-api
-//https://vuelidate-next.netlify.app/examples.html#form-submission
+import { maxLength } from '@vuelidate/validators'
+
 export default {
   setup() {
     return { v$: useVuelidate() }
   },
   data() {
     return {
-      file: '',
-      description: ''
+      form: {
+        file: '',
+        description: ''
+      }
     }
   },
+
   validations() {
     return {
-      // file:
-      // description:
+      form: {
+        file: {
+          maxValue: (file) => {
+            return file.size > 512000 ? false : true
+          }
+        },
+
+        description: {
+          maxLength: maxLength(1000)
+        }
+      }
+    }
+  },
+  methods: {
+    handleFileUpload(event) {
+      this.form.file = event.target.files[0]
+    },
+    submitForm(event) {
+      this.v$.$validate()
     }
   }
 }
@@ -35,22 +55,38 @@ export default {
     </div>
 
     <div class="mb-3 mt-4">
-      <form>
-        <input
-          class="form-control"
-          type="file"
-          id="formFile"
-          accept="image/png,image/gif,image/jpeg"
-        />
-        <div class="form-text text-danger">Image size must be less than 500ko</div>
-        <div class="form-text mb-3">Photo, avatar or any image.</div>
-
-        <label for="description" class="form-label">Description</label>
-        <textarea name="description" id="description" class="form-control" rows="10"></textarea>
-        <div class="form-text">Text with a maximum of 1000 chars.</div>
-
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-4">
-          <button class="btn btn-primary me-md-2" type="submit">Update</button>
+      <form @submit.prevent="submitForm">
+        <div>
+          <input
+            v-on:change="handleFileUpload"
+            name="formFile"
+            id="formFile"
+            class="form-control"
+            type="file"
+            accept="image/png,image/gif,image/jpeg"
+          />
+          <div class="input-errors" v-for="(error, index) of v$.form.file.$errors">
+            <div class="form-text text-danger">Image size must be less than 500ko</div>
+          </div>
+          <div class="form-text mb-3">Photo, avatar or any image.</div>
+        </div>
+        <div :class="{ error: v$.form.description.$errors.maxLength }">
+          <label for="description" class="form-label">Description</label>
+          <textarea
+            v-model="v$.form.description.$model"
+            name="description"
+            id="description"
+            class="form-control"
+            rows="10"
+          ></textarea>
+          <div class="input-errors" v-for="(error, index) of v$.form.description.$errors">
+            <div class="form-text error-msg text-danger">Text with a maximum of 1000 chars .</div>
+          </div>
+        </div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-4 buttons-w">
+          <button class="btn btn-primary me-md-2" type="submit" :disabled="v$.form.invalid">
+            Update
+          </button>
         </div>
       </form>
     </div>
